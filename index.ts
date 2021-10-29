49875e1f11ffd409f5a5870a79d999e6d2a83803
Sonyčka Bičíková <alina9601.1@gmail.com>
2021-10-29 10:28:11 +0200

    First commit

---
+++ index.ts
@@ -0,0 +1,41 @@
+import DiscordJS, { Intents, Message, ReactionEmoji } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.on('ready', () => {
+    console.log('The bot is ready')
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'pong'
+        })
+    }
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'bingus') {
+        message.reply({
+            content: 'bongus'
+        })
+    }
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'good boi bingus') {
+        message.reply({
+            content: 'meow'
+        })
+    }
+})
+
+
+client.login(process.env.TOKEN)
